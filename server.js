ac4ab825a7208088bb4cfcf2329af063c2b7bdeb
NiveenALSmadi <niveensmadi33@gmail.com>
2021-04-25 14:40:33 +0300

    done lab06

--- server.js
+++ server.js
@@ -5,48 +5,48 @@ const PORT =process.env.PORT || 9000;
 
 
 server.get('/location',(req,res)=>{
-    const locdata=require('./data/location.json')
-    let locObject= new Location(locdata);
-    res.send(locObject)
-})
+  const locdata=require('./data/location.json');
+  let locObject= new Location(locdata);
+  res.send(locObject);
+});
 
 let weatherArr=[];
 server.get('/weather',(req,res)=>{
-const weatherData=require('./data/weather.json')
+  const weatherData=require('./data/weather.json');
 
-   weatherData.data.forEach(element => {
+  weatherData.data.forEach(element => {
     let date =element.valid_date;
-    let description=element.weather.description
-    let weather=new Weather(description,date)
+    let description=element.weather.description;
+    let weather=new Weather(description,date);
     weatherArr.push(weather);
+  });
+  res.send(weatherArr);
 });
-res.send(weatherArr)
-})
 
 
 function Location(locationData) {
-    this.search_query = 'Lynwood';
-    this.formatted_query = locationData[0].display_name;
-    this.latitude = locationData[0].lat;
-    this.longitude = locationData[0].lon;
+  this.search_query = 'Lynwood';
+  this.formatted_query = locationData[0].display_name;
+  this.latitude = locationData[0].lat;
+  this.longitude = locationData[0].lon;
 }
 
 function Weather(forecast, time) {
-    this.forecast = forecast;
-    this.time = time;
+  this.forecast = forecast;
+  this.time = time;
 }
 
 
 server.get('*', (req, res) => {
-    let errObject = {
-      status: 500,
-      responseText: 'Sorry, something went wrong',
-    };
-    res.status(500).send(errObject);
-  });
+  let errObject = {
+    status: 500,
+    responseText: 'Sorry, something went wrong',
+  };
+  res.status(500).send(errObject);
+});
 
 
 server.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}`);
-})
+  console.log(`listening on port ${PORT}`);
+});
 
