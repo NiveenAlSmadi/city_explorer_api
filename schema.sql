94ccc93e0f6e51ba563e85709c8df52e25e7225e
NiveenALSmadi <niveensmadi33@gmail.com>
2021-04-27 22:39:44 +0300

    done

--- schema.sql
+++ schema.sql
@@ -7,14 +7,3 @@ CREATE TABLE IF NOT EXISTS loco (
   longitude  VARCHAR(255)
 
 );
-
-
--- DROP TABLE IF EXISTS locations;
-
--- CREATE TABLE locations (
---     id SERIAL PRIMARY KEY,
---     search_query VARCHAR(255),
---     formatted_query VARCHAR(255),
---     latitude NUMERIC(10, 7),
---     longitude NUMERIC(10, 7)
---   );
